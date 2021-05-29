fd5006392c505f760c8db38ee7659ce6522560cc
byunguk.choi <qowlz45@gmail.com>
2021-05-29 22:47:56 +0900

    20. rendering list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, ScrollView } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView, View } from 'react-native';
 
 // after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -10,42 +10,21 @@ export default class App extends React.Component {
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.container}>
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
+          <View style={styles.item}>
+            <Text style={styles.text}>Apple</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Banana</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Cherries</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Damson plum</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>...</Text>
+          </View>
         </ScrollView>
         <StatusBar style="auto" />
       </SafeAreaView>
@@ -56,5 +35,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  item: {
+    flex: 1,
+    height: 50,
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'orange',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
