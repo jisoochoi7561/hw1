c2712b84f38a403784e3afee6ddcc30130e57a9b
byunguk.choi <qowlz45@gmail.com>
2021-05-29 20:55:00 +0900

    04.button and inline handler

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textBig}>Hello World!</Text>
-	  <Text style={[styles.textBig, styles.textRed]}>Hello, world!</Text>
+      <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,11 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-	textBig: {
-		fontSize:32,
-	},
-	textRed:{
-		color:'red'
-	},
+  }
 });
