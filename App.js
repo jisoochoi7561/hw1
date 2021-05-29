bdce4d6096bb76f3d05e8cef073f9d6ccabcef42
Seongmin Kim <matthewsmkim@naver.com>
2021-05-29 22:41:23 +0900

    15. Stretch and flex ratio

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>1</Text>
-        <Text style={styles.text}>2</Text>
+        <Text style={[styles.text, styles.text2]}>2</Text>
         <Text style={styles.text}>3</Text>
         <StatusBar style="auto" />
       </View>
@@ -19,13 +19,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
-    flexDirection: 'row',
+    
   },
   text: {
     flex: 1,
     borderWidth: 1,
     borderColor: 'red',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
+  text2: {
+    flex: 2,
+  }
 });
