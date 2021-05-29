7ad1bef58bf76f5ce7d98cc87705b62183b6e0cb
jisoo <jisoochoi7561@gmail.com>
2021-05-29 23:15:26 +0900

    21~24

--- App.js
+++ App.js
@@ -1,33 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import React from 'react';
-import { Image, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 // after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
+  fruits = [
+    { name: 'Acai' },
+    { name: 'Apples' },
+    { name: 'Apricots' },
+    { name: 'Avocado' },
+    { name: 'Ackee' },
+    { name: 'Bananas' },
+    { name: 'Bilberries' },
+    { name: 'Blueberries' },
+    { name: 'Blackberries' },
+    { name: 'Boysenberries' },
+    { name: 'Bread fruit' },
+    { name: 'Cantaloupes (cantalope)' },
+    { name: 'Chocolate-Fruit' },
+    { name: 'Cherimoya' },
+    { name: 'Cherries' },
+    { name: 'Cranberries' },
+    { name: 'Cucumbers' },
+    { name: 'Currants' },
+    { name: 'Dates' },
+    { name: 'Durian' },
+    { name: 'Eggplant' },
+    { name: 'Elderberries' },
+    { name: 'Figs' },
+    { name: 'Gooseberries' },
+    { name: 'Grapes' },
+    { name: 'Grapefruit' },
+    { name: 'Guava' },
+    { name: 'Honeydew melons' },
+    { name: 'Horned melon (Kiwano)' },
+    { name: 'Huckleberries' },
+    { name: 'Ita Palm' },
+    { name: 'Jujubes' },
+    { name: 'Kiwis' },
+    { name: 'Durian is an unusual tropical fruit.' },
+    { name: 'Kumquat' },
+    { name: 'Lemons' },
+    { name: 'Limes' },
+    { name: 'Lychees' },
+    { name: 'Mangos' },
+    { name: 'Mangosteen' },
+    { name: 'Mulberries' },
+    { name: 'Muskmelon' },
+    { name: 'Nectarines' },
+    { name: 'Ogden melons' },
+    { name: 'Olives' },
+    { name: 'Oranges' },
+    { name: 'Papaya' },
+    { name: 'Passion fruit' },
+    { name: 'Peaches' },
+    { name: 'Pears' },
+    { name: 'Peppers' },
+    { name: 'Persimmon' },
+    { name: 'Pineapple' },
+    { name: 'Plums' },
+    { name: 'Pluot' },
+    { name: 'Pomegranate' },
+    { name: 'Prickly Pear' },
+    { name: 'Quince' },
+    { name: 'Rambuton' },
+    { name: 'Raspberries' },
+    { name: 'Rose Apple' },
+    { name: 'Starfruit' },
+    { name: 'Sapadilla' },
+    { name: 'Strawberries' },
+    { name: 'Tamarind' },
+    { name: 'Tangelo' },
+    { name: 'Tangerines' },
+    { name: 'Tomatoes' },
+    { name: 'Ugli fruit' },
+    { name: 'Voavanga (Spanish Tamarind)' },
+    { name: 'Watermelons' },
+    { name: 'Xigua melon' },
+    { name: 'Yellow watermelon' },
+    { name: 'Zucchini' },
+  ];
+
+  renderItem({ name }) {
+    return (
+      <View style={styles.item} >
+        <Text style={styles.text}>{name}</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
+		<SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.container}>
-          <View style={styles.item}>
-            <Text style={styles.text}>Apple</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Banana</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Cherries</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Damson plum</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>...</Text>
-          </View>
-        </ScrollView>
+         <FlatList style={styles.container}
+		 keyExtractor={(item) => item.name}
+           renderItem={({ item }) => this.renderItem(item)}
+           data={this.fruits}
+         />
         <StatusBar style="auto" />
       </SafeAreaView>
+	  </SafeAreaProvider>
     );
   }
 }
