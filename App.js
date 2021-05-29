cfcc5993920a3fe8b214d5705a058a122993e462
jisoo <jisoochoi7561@gmail.com>
2021-05-30 00:30:08 +0900

    30. react navi

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import * as React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import CityList from './CityList';
 
 // after `expo install react-native-safe-area-context`.
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+const HomeScreen = () => (
+   <View style={styles.container}>
+     <CityList/>
+     <StatusBar style="auto" />
+   </View>
+ );
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <CityList/>
-          <StatusBar style="auto" />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    );
-  }
-}
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+
+export default class App extends React.Component {
+   render() {
+     return (
+       <NavigationContainer>
+         <Stack.Navigator>
+           <Stack.Screen
+             name="Home"
+             component={HomeScreen}
+           />
+         </Stack.Navigator>
+       </NavigationContainer>
+     );
+   }
+ } 
