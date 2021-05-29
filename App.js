a330b4c5cad10cf9a624ea786dd2a39828c6c105
Seongmin Kim <matthewsmkim@naver.com>
2021-05-29 22:43:59 +0900

    16. Image and border for detecting edges

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>1</Text>
-        <Text style={[styles.text, styles.text2]}>2</Text>
-        <Text style={styles.text}>3</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
         <StatusBar style="auto" />
       </View>
     );
@@ -19,20 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
+    borderWidth: 2,
     justifyContent: 'center',
-    
+    alignItems: 'center',
   },
-  text: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  text2: {
-    flex: 2,
-  }
 });
